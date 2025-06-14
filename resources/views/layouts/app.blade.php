46036e029c31a330fc83c3834d9150aed6bb407e
sukma <Ragasukma23@gmail.com>
2025-06-14 15:06:55 +0700

    hmm

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -89,7 +89,7 @@
             position: fixed;
             top: 0; left: 0; right: 0; bottom: 0;
             background: rgba(0,0,0,0.2);
-            z-index: 1049;
+            z-index: 1050;
         }
         #sidebar.show ~ #sidebar-backdrop {
             display: block;
